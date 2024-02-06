1a8885fd2c3ab9e0306d3a2a3ea965fe47807a3e
ELLINM <ellinm913043@gmail.com>
2024-02-06 22:50:51 +0900

    First Commit Create DropDown

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+  import DropDown from '@/components/DropDown.vue'
+
+  export default {
+  name: 'App',
+  data(){
+    return {
+    }
+  },
+
+  methods : {
+
+  },
+  components: {
+    DropDown : DropDown,
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      Practice Vue
+    </div>
+  </header>
+  <body>
+    <DropDown />
+  </body>
+</template>
+
+<style scoped>
+</style>
